f48eaa029a8964364c24c8ffedc15664ea2d976c
ugurkiymetli <ugurkiymetli@hotmail.com>
2021-10-01 19:52:57 +0300

    filtreleme fonksiyonu eklendi. telefon rehberine mock data yazıldı!

--- src/Components/Contacts/List/index.js
+++ src/Components/Contacts/List/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 //contacts = { fullname: 'string', phone_number: number }
 function List({ contacts }) {
+  const [filterText, setfilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+    );
+  });
+
   return (
     <div>
       <h2>Telefon Rehberi</h2>
+      <input
+        placeholder="Rehberde Ara"
+        value={filterText}
+        onChange={(e) => setfilterText(e.target.value)}
+      />
       <ul>
-        {contacts.map((contact, index) => (
+        {filtered.map((contact, index) => (
           <li key={index}>
-            {contact.fullname} - {contact.phone_number}{" "}
+            {contact.fullname} -
+            <small>
+              <small> {contact.phone_number}</small>
+            </small>
           </li>
         ))}
       </ul>
--- src/Components/Contacts/index.js
+++ src/Components/Contacts/index.js
@@ -3,7 +3,68 @@ import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Lance Harpur",
+      phone_number: "4527243739",
+    },
+    {
+      fullname: "Melicent Lynas",
+      phone_number: "8152229462",
+    },
+    {
+      fullname: "Sukey Aberdeen",
+      phone_number: "8729191366",
+    },
+    {
+      fullname: "Bianka Ditchett",
+      phone_number: "4804856208",
+    },
+    {
+      fullname: "Zulema Trumble",
+      phone_number: "3869850022",
+    },
+    {
+      fullname: "Reuven Dabrowski",
+      phone_number: "8121090982",
+    },
+    {
+      fullname: "Cynthy Walstow",
+      phone_number: "3324248068",
+    },
+    {
+      fullname: "Marchall Caston",
+      phone_number: "5337336410",
+    },
+    {
+      fullname: "Erinn Corkhill",
+      phone_number: "3993063580",
+    },
+    {
+      fullname: "Marty Scrase",
+      phone_number: "5281300317",
+    },
+    {
+      fullname: "Webb Crutchfield",
+      phone_number: "4363661083",
+    },
+    {
+      fullname: "Orly Graysmark",
+      phone_number: "2504609246",
+    },
+    {
+      fullname: "Brandi Ambrosetti",
+      phone_number: "5634823286",
+    },
+    {
+      fullname: "Zachery Channing",
+      phone_number: "2111125976",
+    },
+    {
+      fullname: "Merl Gilstoun",
+      phone_number: "3023305425",
+    },
+  ]);
   useEffect(() => {
     console.log(contacts);
   }, [contacts]);
