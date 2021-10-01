40f502cf5b04956efcbb56655462be8de07139b1
ugurkiymetli <ugurkiymetli@hotmail.com>
2021-10-01 19:12:38 +0300

    form componenti güncellendi. isim ve telefon numarası inputları oluşturuldu. submit fonksiyonu oluşturuldu. (index.html ve manifest.json dosyalarındaki iconlarla alakalı satırlar silindi.)

--- src/Components/Contacts/Form/index.js
+++ src/Components/Contacts/Form/index.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+const initialFormValues = { fullname: "", phone_number: "" };
+function Form({ addContact, contacts }) {
+  //formun ilk boş inputlarını oluşturduk
 
-function Form() {
-  return <div>Contact Form</div>;
+  useEffect(() => {
+    //form inputlarını temizledik.
+    setForm(initialFormValues);
+  }, [contacts]);
+
+  //form state
+  const [form, setForm] = useState(initialFormValues);
+
+  useEffect(() => {
+    console.log("Form Component Mounted!");
+  }, []);
+  // forma veri girdiğinde stateler güncelleniyor.
+  const onFormChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  //veriyi contacts'e ekliyoruz
+  //eğer boş ise false dönüyor.
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullname === "" || form.phone_number === "") {
+      console.log("isim veya telefon numarası boş olamaz.");
+      return false;
+    }
+    //önceki datayı tutup üzerine ekledik.
+    addContact([...contacts, form]);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="İsim Soyisim"
+          value={form.fullname}
+          onChange={onFormChange}
+        />
+      </div>
+      <div>
+        <input
+          name="phone_number"
+          placeholder="Telefon Numarası"
+          value={form.phone_number}
+          onChange={onFormChange}
+        />
+      </div>
+      <div>
+        <button>Rehbere Ekle</button>
+      </div>
+    </form>
+  );
 }
+//proptypes tanımları
+Form.propTypes = {
+  fullname: PropTypes.string,
+  phone_number: PropTypes.number,
+};
 
 export default Form;
