ae568e2a42158e5ad36190bd233817b72987877a
ugurkiymetli <ugurkiymetli@hotmail.com>
2021-10-01 17:30:37 +0300

    contacts-app first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Contacts from "./Components/Contacts";
+function App() {
+  return (
+    <div className="App">
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
